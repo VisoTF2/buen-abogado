3dda75d1623d85c3d5e27596b7f1ab8be13864de
VisoTF2 <vicentesotofredes@gmail.com>
2026-04-06 00:39:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -21,9 +21,6 @@ async function createWindow() {
 
   win.loadURL("http://127.0.0.1:3000");
 
-  // Abrir DevTools para debugging
-  win.webContents.openDevTools();
-
   win.setMenuBarVisibility(false);
 }
 
